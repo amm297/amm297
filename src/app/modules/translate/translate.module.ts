2db95a2596e6563c4a953275e26f8febd9763f91
Alberto <alberto.martinez.martinez.1@gmail.com>
2020-04-19 18:56:57 +0200

    Deploy updated

--- src/app/modules/translate/translate.module.ts
+++ src/app/modules/translate/translate.module.ts
@@ -24,5 +24,5 @@ export class AppTranslateModule {}
 
 // required for AOT compilation
 export function HttpLoaderFactory(http: HttpClient) {
-  return new TranslateHttpLoader(http);
+  return new TranslateHttpLoader(http, "./assets/i18n/", ".json");
 }
